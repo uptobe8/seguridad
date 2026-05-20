8e3a52882e06de938a188732f8d7a66012266ebd
uptobe8 <ia.uptobe@gmail.com>
2026-05-20 14:44:41 +0200

    Publish app fallback files

---
+++ docs/config.js
@@ -0,0 +1,10 @@
+window.APP_CONFIG = {
+  teacherPassword: 'profesor2026',
+  github: {
+    enabled: false,
+    owner: 'uptobe8',
+    repo: 'seguridad',
+    token: '',
+    label: 'resultado-alumno'
+  }
+};
